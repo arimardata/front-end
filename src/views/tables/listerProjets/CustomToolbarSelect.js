7914728229739c329802677d005427b10dc37b1c
hamza enaime <hamza.enaime@outlook.com>
2019-09-04 17:59:36 +0100

    show the information modal another way

--- src/views/tables/listerProjets/CustomToolbarSelect.js
+++ src/views/tables/listerProjets/CustomToolbarSelect.js
@@ -1,22 +1,43 @@
 import React from "react";
-import IconButton from "@material-ui/core/IconButton";
-import Tooltip from "@material-ui/core/Tooltip";
 import CompareArrowsIcon from "@material-ui/icons/CompareArrows";
 import IndeterminateCheckBoxIcon from "@material-ui/icons/IndeterminateCheckBox";
-import BlockIcon from "@material-ui/icons/Block";
-import { withStyles } from "@material-ui/core/styles";
 import { Store, Constants, Dispatcher } from "../../../flux";
 import fetchApi from "../../../utils/fetchApi";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
-import { Info } from "@material-ui/icons";
 import UpdateMaterielModal from "../../../utils/UpdateMaterielModal";
 
 import ConfirmDeleteModal from "../../../utils/ConfirmDeleteModal";
 
+import { Modal, ModalBody } from "shards-react";
+
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+  useMediaQuery,
+  Slide,
+  AppBar,
+  Toolbar,
+  Typography,
+  Tooltip,
+  IconButton,
+  Chip
+} from "@material-ui/core";
+
+import CloseIcon from "@material-ui/icons/Close";
+
+import { Info } from "@material-ui/icons";
+
 import Informations from "./Modals/Informations";
 
-import { Modal, ModalBody } from "shards-react";
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const defaultToolbarSelectStyles = {
   iconContainer: {
@@ -31,7 +52,13 @@ const defaultToolbarSelectStyles = {
 };
 
 class CustomToolbarSelect extends React.Component {
-  state = { open: false, openDeleteDialog: false, openInformations: false };
+  state = {
+    open: false,
+    openDeleteDialog: false,
+    openInformations: false,
+    fullScreen: false
+  };
+
   handleClickInverseSelection = () => {
     const nextSelectedRows = this.props.displayData.reduce(
       (nextSelectedRows, _, index) => {
@@ -49,6 +76,20 @@ class CustomToolbarSelect extends React.Component {
     );
 
     this.props.setSelectedRows(nextSelectedRows);
+  };
+
+  handleOpen = () => {
+    console.log(this.props);
+    this.setState({ openInformations: true });
+  };
+
+  handleClose = () => {
+    this.setState({ openInformations: false });
+  };
+
+  handleFullscreen = () => {
+    let fullScreen = !this.state.fullScreen;
+    this.setState({ fullScreen });
   };
 
   delete = () => {
@@ -99,13 +140,10 @@ class CustomToolbarSelect extends React.Component {
       openDeleteDialog: !this.state.openDeleteDialog
     });
   };
-  handleOpen = () => {
-    this.setState({
-      openInformations: !this.state.openInformations
-    });
-  };
+
   render() {
     const { classes } = this.props;
+    const { openInformations, fullScreen } = this.state;
     let info;
 
     if (this.props.selectedRows.data.length == 1)
@@ -126,7 +164,6 @@ class CustomToolbarSelect extends React.Component {
             </IconButton>
           </Tooltip>
           {info}
-          {this.state.openInformations && <Informations />}
           <Modal
             open={this.state.openDeleteDialog}
             className={classes.deleteDialogTop}
@@ -140,6 +177,66 @@ class CustomToolbarSelect extends React.Component {
             </ModalBody>
           </Modal>
         </React.Fragment>
+        <div>
+          <Dialog
+            maxWidth={"md"}
+            fullWidth={!fullScreen}
+            fullScreen={fullScreen}
+            open={openInformations}
+            keepMounted
+            onClose={this.handleClose}
+            TransitionComponent={Transition}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            {fullScreen && (
+              <AppBar className={classes.appBar}>
+                <Toolbar>
+                  <IconButton
+                    color="inherit"
+                    onClick={this.handleClose}
+                    aria-label="Fermer"
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                  <IconButton
+                    color="inherit"
+                    onClick={this.handleFullscreen}
+                    aria-label="FullScreen"
+                  >
+                    <i class="material-icons">minimize</i>
+                  </IconButton>
+                </Toolbar>
+              </AppBar>
+            )}
+            <DialogTitle id="alert-dialog-title">
+              <IconButton
+                color="inherit"
+                onClick={this.handleClose}
+                aria-label="Fermer"
+              >
+                <CloseIcon />
+              </IconButton>
+              <IconButton
+                color="inherit"
+                onClick={this.handleFullscreen}
+                aria-label="FullScreen"
+              >
+                <i class="material-icons">fullscreen</i>
+              </IconButton>
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                <Informations />
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Fermer
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
       </div>
     );
   }
